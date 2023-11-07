8afc13804de7e896a59ea22d5bcd383d44ad86e0
Priyanka-Das-Dipa <priyankadipa9102@gmail.com>
2023-11-08 00:22:28 +0600

    add modal to the book button

--- src/Pages/ServiceDetails.jsx
+++ src/Pages/ServiceDetails.jsx
@@ -4,7 +4,16 @@ import { Link, useParams } from "react-router-dom";
 const ServiceDetails = () => {
   const { id } = useParams();
   const [detailData, setDetailData] = useState({});
+  const [isModalOpen, setIsModalOpen] = useState(false);
   console.log(detailData);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
   useEffect(() => {
     fetch(`http://localhost:5000/services/${id}`)
       .then((res) => res.json())
@@ -55,10 +64,69 @@ const ServiceDetails = () => {
           </div>
           <div className="my-5">
             <Link>
-              <button className="btn w-full btn-primary">Book Now</button>
+              <button onClick={openModal} className="btn w-full btn-primary">
+                Book Now
+              </button>
             </Link>
           </div>
         </div>
+        {isModalOpen && (
+          <div className="fixed inset-0 flex items-center justify-center z-50">
+            <div className="flex flex-col max-w-md gap-2 p-6 text-white
+             rounded-md shadow-md bg-purple-500 dark:text-gray-100">
+              <h2 className="flex items-center gap-2 text-xl font-semibold leadi tracki">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  className="w-6 h-6 fill-current shrink-0 dark:text-violet-400"
+                >
+                  <path d="M451.671,348.569,408,267.945V184c0-83.813-68.187-152-152-152S104,100.187,104,184v83.945L60.329,348.568A24,24,0,0,0,81.432,384h86.944c-.241,2.636-.376,5.3-.376,8a88,88,0,0,0,176,0c0-2.7-.135-5.364-.376-8h86.944a24,24,0,0,0,21.1-35.431ZM312,392a56,56,0,1,1-111.418-8H311.418A55.85,55.85,0,0,1,312,392ZM94.863,352,136,276.055V184a120,120,0,0,1,240,0v92.055L417.137,352Z"></path>
+                  <rect width="32" height="136" x="240" y="112"></rect>
+                  <rect width="32" height="32" x="240" y="280"></rect>
+                </svg>
+                Are You sure went to BOOK this?
+              </h2>
+              <p className="flex-1 dark:text-gray-400">
+                <strong>Service Name:</strong> {detailData.serviceName}
+                <br />
+                <strong>Service Image:</strong>{" "}
+                <img
+                  src={detailData.serviceImage}
+                  alt=""
+                  className="w-16 h-16 object-cover"
+                />
+                <br />
+                <strong>Service Provider Email:</strong>{" "}
+                {detailData.serviceProvider?.email}
+                <br />
+                <strong>User Email:</strong> 
+                {/* //Add user email here */}
+                <br />
+                <strong>Service Taking Date:</strong>{" "}
+                <input
+                className="text-black w-full"
+                  type="date"
+                  // value={detailData.serviceTakingDate}
+                  // onChange={(e) => {}}
+                />
+                <br />
+                <strong>Special Instruction:</strong>{" "}
+                <input
+                  type="text"
+                  className="text-black w-full"
+                  // value={detailData.specialInstruction}
+                  // onChange={(e) => {}}
+                />
+              </p>
+              <div className="flex flex-col justify-end gap-3 mt-6 sm:flex-row">
+                
+                <button onClick={closeModal} className="px-6 btn py-2 rounded-2xl shadow-sm dark:bg-violet-400 dark:text-gray-900">
+                Purchase
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
